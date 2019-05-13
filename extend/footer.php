613c565730d940c6bc5e05cc4e2774641f5cb5b1
Gary <jvermelho@prontonmail.com>
2019-05-13 00:47:38 -0500

    aun no funciona el pto login

--- extend/footer.php
+++ extend/footer.php
@@ -4,5 +4,6 @@
   <script src="../js/bootstrap.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
+  <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js"></script>
   <script src="js/app.js"></script> <!--firebase auth-->
   
--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyCNNqUU6jOvObHVbU5qxRiEjYbkcRkg6Io",
     authDomain: "mqfast-c6e6e.firebaseapp.com",
@@ -8,24 +7,24 @@ var firebaseConfig = {
     messagingSenderId: "1083184136645",
     appId: "1:1083184136645:web:bd9704a4845a5e7c"
 };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const txtEmail = document.getElementById('correo');
-const txtPassword = document.getElementById('password');
-const btnLogin = document.getElementById('btnlogin');
+const txtPassword = document.getElementById('pass');
+const btnLogin = document.getElementById('login');
 
 btnLogin.addEventListener('click', e => {
     const email = txtEmail.value;
     const pass = txtPassword.value;
-    const auth = btnLogin.auth();
+    const auth = firebase.auth();
 
     const promesa = auth.signInWithEmailAndPassword(email,pass);
-    promesa.catch(e => location.href = "admin/error.php") //en el caso de que no se validen los datos te mandara a esa ruta
+    //promesa.catch(e => location.href = "admin/error.php"); //en el caso de que no se validen los datos te mandara a esa ruta    
+    promesa.catch(e => console.log(e.message));
 });
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
+firebase.auth().onAuthStateChanged(firebaseUser =>{
+    if (firebaseUser) {
         location.href="admin";
     }
 });
